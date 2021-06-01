5b70ace56f698b5c749b61b28ab4291624b92ba4
NikiLeonVladis <84407537+NikiLeonVladis@users.noreply.github.com>
2021-06-01 22:00:11 +0300

    Комент

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,6 +1,6 @@
 package com.company;
 
-public class Main {
+public class Main {                          //Как-то так в общем
 
     public static void main(String[] args) {
 	// write your code here
